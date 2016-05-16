ca6ba64722379f66b0ea817a864b2ae660e45c83
Sameen Islam <ec14370@qmul.ac.uk>
2016-05-16 20:05:46 +0100

    added hyper SQL utility class

---
+++ com/technologyconversations/books/src/main/java/HibernateUtil.java
@@ -0,0 +1,27 @@
+package com.technologyconversations.books;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+
+public class HibernateUtil {
+
+    private static SessionFactory sessionFactory = buildSessionFactory();
+    private static ServiceRegistry serviceRegistry;
+
+    private static SessionFactory buildSessionFactory() {
+//        return new Configuration().configure().buildSessionFactory();
+        Configuration configuration = new Configuration();
+        configuration.configure();
+        serviceRegistry = new StandardServiceRegistryBuilder().applySettings(
+                configuration.getProperties()).build();
+        sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+        return sessionFactory;
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+
+}
