feca56108a7a87a405aa9d7a14477447d992d4e3
Sameen Islam <ec14370@qmul.ac.uk>
2016-05-16 20:02:43 +0100

    added hyper SQL configuration file

--- com/technologyconversations/books/src/main/java/Book.java
+++ com/technologyconversations/books/src/main/java/Book.java
@@ -3,61 +3,70 @@ import java.io.Serializable;
 /**
  * Created by sameenislam on 16/05/2016.
  */
+@Entity
+@Table(name = "Book")
+@XmlRootElement
 public class Book implements Serializable {
 
-    private int id;
-    private String imageUrl; // the URL to the item cover art
-    private String title; // the title of the item
-    private String author; // the author of the item
-    private String price; // the price of the item
-    private String link; // the link to the item details
+    public Book() { }
 
+    public Book(int id) {
+        setId(id);
+    }
+
+    @Id
+    @Column(name = "id")
+    private int id;
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Column(name = "image")
+    private String imageUrl; // the URL to the item cover art
     public String getImageUrl() {
         return imageUrl;
     }
-
     public void setImageUrl(String imageUrl) {
         this.imageUrl = imageUrl;
     }
 
+    @Column(name = "title")
+    private String title; // the title of the item
     public String getTitle() {
         return title;
     }
-
     public void setTitle(String title) {
         this.title = title;
     }
 
+    @Column(name = "author")
+    private String author; // the author of the item
     public String getAuthor() {
         return author;
     }
-
     public void setAuthor(String author) {
         this.author = author;
     }
 
+    @Column(name = "price")
+    private String price; // the price of the item
     public String getPrice() {
         return price;
     }
-
     public void setPrice(String price) {
         this.price = price;
     }
 
+    @Column(name = "link")
+    private String link; // the link to the item details
     public String getLink() {
         return link;
     }
-
     public void setLink(String link) {
         this.link = link;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
     }
 
     @Override
@@ -67,13 +76,25 @@ public class Book implements Serializable {
 
         Book book = (Book) o;
 
-        if(id != book.id) return false;
-        if(imageUrl != book.imageUrl) return false;
-        if(title != book.title) return false;
-        if(author != book.author) return false;
-        if(price != book.price) return false;
-        if(link != book.link) return false;
+        if (id != book.id) return false;
+        if (Double.compare(book.price, price) != 0) return false;
+        if (author != null ? !author.equals(book.author) : book.author != null) return false;
+        if (image != null ? !image.equals(book.image) : book.image != null) return false;
+        if (title != null ? !title.equals(book.title) : book.title != null) return false;
 
+        return true;
+    }
 
+    @Override
+    public int hashCode() {
+        int result;
+        long temp;
+        result = id;
+        result = 31 * result + (image != null ? image.hashCode() : 0);
+        result = 31 * result + (title != null ? title.hashCode() : 0);
+        result = 31 * result + (author != null ? author.hashCode() : 0);
+        temp = Double.doubleToLongBits(price);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        return result;
     }
 }
---
+++ com/technologyconversations/books/src/test/java/BookTest.java
@@ -0,0 +1,106 @@
+
+public class BookTest extends CommonTest {
+
+    @Test
+    public void idShouldHaveSetterAndGetter() {
+        int expected = 123;
+        book.setId(expected);
+        assertThat(book.getId(), is(equalTo(expected)));
+    }
+
+    @Test
+    public void imageShouldHaveSetterAndGetter() {
+        String expected = "new_path/image.png";
+        book.setImage(expected);
+        assertThat(book.getImage(), is(equalTo(expected)));
+    }
+
+    @Test
+    public void titleShouldHaveSetterAndGetter() {
+        String expected = "new title";
+        book.setTitle(expected);
+        assertThat(book.getTitle(), is(equalTo(expected)));
+    }
+
+    @Test
+    public void authorShouldHaveSetterAndGetter() {
+        String expected = "new author";
+        book.setAuthor(expected);
+        assertThat(book.getAuthor(), is(equalTo(expected)));
+    }
+
+    @Test
+    public void priceShouldHaveSetterAndGetter() {
+        double expected = 123.45;
+        book.setPrice(expected);
+        assertThat(book.getPrice(), is(equalTo(expected)));
+    }
+
+    @Test
+    public void linkShouldHaveSetterAndGetter() {
+        String expected = "new link";
+        book.setLink(expected);
+        assertThat(book.getLink(), is(equalTo(expected)));
+    }
+
+    @Test
+    public void equalsShouldFailIfIdIsNotTheSame() {
+        Book actual = new Book(123);
+        actual.setImage(image);
+        actual.setTitle(title);
+        actual.setAuthor(author);
+        actual.setPrice(price);
+        assertThat(actual, is(not(equalTo(book))));
+    }
+
+    @Test
+    public void equalsShouldReturnFalseIfImageIsNotTheSame() {
+        Book actual = new Book(id);
+        actual.setImage("new image");
+        actual.setTitle(title);
+        actual.setAuthor(author);
+        actual.setPrice(price);
+        assertThat(actual, is(not(equalTo(book))));
+    }
+
+    @Test
+    public void equalsShouldReturnFalseIfTitleIsNotTheSame() {
+        Book actual = new Book(id);
+        actual.setImage(image);
+        actual.setTitle("new title");
+        actual.setAuthor(author);
+        actual.setPrice(price);
+        assertThat(actual, is(not(equalTo(book))));
+    }
+
+    @Test
+    public void equalsShouldReturnFalseIfAuthorIsNotTheSame() {
+        Book actual = new Book(id);
+        actual.setImage(image);
+        actual.setTitle(title);
+        actual.setAuthor("new author");
+        actual.setPrice(price);
+        assertThat(actual, is(not(equalTo(book))));
+    }
+
+    @Test
+    public void equalsShouldReturnFalseIfPriceIsNotTheSame() {
+        Book actual = new Book(id);
+        actual.setImage(image);
+        actual.setTitle(title);
+        actual.setAuthor(author);
+        actual.setPrice(123.45);
+        assertThat(actual, is(not(equalTo(book))));
+    }
+
+    @Test
+    public void equalShouldReturnTrueIfIdImageTitleAuthorAndPriceAreTheSame() {
+        Book actual = new Book(id);
+        actual.setImage(image);
+        actual.setTitle(title);
+        actual.setAuthor(author);
+        actual.setPrice(price);
+        assertThat(actual, is(equalTo(book)));
+    }
+
+}
